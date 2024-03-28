1a2a9131c1d97e83f4d5889d8725d3fc200930d3
sseker665 <sseker665@gmail.com>
2024-03-28 15:21:07 +0300

    added min max function with np function

--- company/company_np.py
+++ company/company_np.py
@@ -1,5 +1,31 @@
 import numpy as np
 from operationClass import IntArray
+
+def productivity_of_company(order,data_frame):
+
+    return np.sum(data_frame[order])
+
+def max_product(data_frame):
+    i = 0
+    bestCompany = i + 1
+    sumProducts = 0
+
+    for i in range(len(data_frame)):
+        result = productivity_of_company(i,data_frame)
+        if result > sumProducts:
+            sumProducts = result
+            bestCompany = i + 1
+    return print(f'The best company is the {bestCompany} with {sumProducts} products')
+def min_product(data_frame):
+    i = 0
+    worstCompany = i + 1
+    sumProducts = productivity_of_company(0,data_frame)
+    for i in range(len(data_frame)):
+        result = productivity_of_company(i,data_frame)
+        if  result <= sumProducts:
+            sumProducts = result
+            worstCompany = i + 1
+    return print(f'The worst company is the {worstCompany} with {sumProducts}')
 def fileHandling():
 
     lines = []
@@ -19,8 +45,9 @@ def main():
     print(data_frame)
 
     firstBranch = IntArray(data_frame[0])
-    firstBranch.display()
-    firstBranch.salary()
-    firstBranch.showData()
-
+    # firstBranch.display()
+    # firstBranch.salary()
+    # firstBranch.showData()
+    min_product(data_frame)
+    max_product(data_frame)
 main()
